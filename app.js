9a539b14749583c3333942cc45e102da9d4e99b0
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-05-03 10:25:51 -0400

    Added makepdf module

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //const fetchReddit = require("./request/fetchReddit.js");
 const reddit = require("./request/reddit");
 const ph = require("./request/producthunt");
-const format = require('./compose/format');
+const makePDF = require("./compose/makePDF");
+const format = require("./compose/format");
 const reddit_url = "https://www.reddit.com/r/popular/top.json?t=day";
 
 const count = 5;
-let printArray = [];
+let pdf_content = [];
 
 let Reddit = new reddit(reddit_url);
 let ProductHunt = new ph(5);
@@ -14,12 +15,16 @@ const print = async () => {
   let r_result = await Reddit.default(5);
   let ph_result = await ProductHunt.default(5);
 
-  let result = r_result.join("\n") + "\n\n" + ph_result.join("\n");
-  format(result);
+  // ****  Old way of printing to file
+  //let result = r_result.join("\n") + "\n\n" + ph_result.join("\n");
+  //format(result);
+  pdf_content.push(r_result.join("\n"))
+  pdf_content.push(ph_result.join("\n"))
+
+  makePDF(pdf_content);
 };
 
 print();
 
-//Reddit.keyboard();
 
 // TODO: Add posts from hacker news
--- compose/makePDF.js
+++ compose/makePDF.js
@@ -1,3 +1,29 @@
-const PDFDocument = require('pdfkit');
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
 
-const doc = new PDFDocument;
+let curr_date = new Date();
+let doc_name = curr_date.toLocaleDateString().concat(" digest.pdf");
+
+function makePDF(content) {
+  const doc = new PDFDocument();
+  doc.pipe(fs.createWriteStream(doc_name));
+
+  doc.fontSize(12).text(`${content[0]}`, 50, 50, {
+    width: 225,
+    alighn: "left",
+  });
+
+  doc.fontSize(10).text(`${content[1]}`, 325, 50, {
+    width: 225,
+    alighn: "right",
+    fontSize: 10,
+  });
+
+  // Finalize the pdf file
+  doc.end();
+  console.log("PDF Created");
+}
+
+//makePDF("test");
+
+module.exports = makePDF;
