6afc9314321f11c558c3325f25127da5f7422b86
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-05-03 09:13:12 -0400

    Fixed duplicate print on restart

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # print-news
-Print a daily report of top posts on Hacker News, Product Hunt, Reddit, etc.
+Print a daily digest of top posts on Hacker News, Product Hunt, Reddit, etc.
--- format.js
+++ format.js
@@ -3,8 +3,9 @@ const fs = require("fs");
 let curr_date = new Date();
 curr_date = curr_date.toLocaleDateString().concat(" digest");
 
+// Writes content String to file named current date
 function format(content) {
-  fs.appendFile(curr_date, content, (err) => {
+  fs.writeFile(curr_date, content, (err) => {
     if (err) throw err;
     console.log("Content saved");
   });
